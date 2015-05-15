3c1ac4083ef2110bdf7ab8a59861e2d80dc575c3
Matthew Savery <mhsavery126@gmail.com>
2015-05-14 21:19:26 -0400

    laid out functions / instantiated Geocoder

--- map.js
+++ map.js
@@ -1,16 +1,46 @@
-$(document).ready(function(){
+$(document).ready(function(){    
+
+    var geocoder;
+    var map;
 
 	function initialize() {
+
+            geocoder = new google.maps.Geocoder();
             var mapOptions = {
                 zoom: 3,
                 center: new google.maps.LatLng(0,0)
             };
-            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+            map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
             var streetViewLayer = new google.maps.StreetViewCoverageLayer();
             streetViewLayer.setMap(map);
-        }
+    }
 
-        google.maps.event.addDomListener(window, 'load', initialize);
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+    $(".location").focus(function(){
+
+
+    });
+
+
+    $(".go-to").click(function(){
+
+
+    });
+
+
+    $(".set").click(function(){
+
+
+    });
+
+
+    $("#createLink").click(function(){
+
+
+    });
 
 
 
